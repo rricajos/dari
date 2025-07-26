d3de0674613a9b0d58329ff43c9a1519e3600883
IFP8 <ergoegos@gmail.com>
2025-07-26 11:17:57 +0200

    install button demo

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
-const CACHE = "darink-cache-v8";
+const CACHE = "darink-cache-v1";
 const ASSETS = [
   "./",
   "./index.html",
   "./ui.js",
   "./storage.js",
-  "./sw-register.js",
   "./manifest.json",
-  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
 ];
+
 self.addEventListener("install", (e) =>
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)))
 );
+
 self.addEventListener("fetch", (e) =>
   e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)))
 );
